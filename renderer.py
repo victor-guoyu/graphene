56b0edc8280189b62778952caaab08963802c06a
Victor Guo <victor.guoyu@hotmail.com>
2019-08-18 15:06:42 -0400

    test data

--- renderer.py
+++ renderer.py
@@ -13,8 +13,8 @@ class PngRender(BaseRenderer):
     def draw(self, graph):
         edge_labels = graph.get_edge_labels()
         nx_graph = graph.nx_graph
-        pos = nx.fruchterman_reingold_layout(nx_graph)
+        pos = nx.circular_layout(nx_graph)
         nx.draw(nx_graph, pos=pos, node_size=40)
         nx.draw_networkx_edge_labels(
-            nx_graph, pos, edge_labels=edge_labels, font_size=3, alpha=0.8)
+            nx_graph, pos, edge_labels=edge_labels, label_pos=0.3, font_size=3, alpha=0.8)
         plt.savefig(self.output_path, dpi=1024)
